47164cb5a513dd8ec219bc6f990b99244215d18e
Takumi Tsutsui <takumitsutsui@TakuminoMac-mini.local>
2026-04-24 05:25:34 +0900

    first

---
+++ lib/sujiko.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sujiko/version"
+
+module Sujiko
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sujiko/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Sujiko
+  VERSION = "0.1.0"
+end
